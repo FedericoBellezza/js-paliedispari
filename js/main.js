9c359d92dd5724afa81abef67b313cb8a7f6e4f3
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-18 15:19:51 +0200

    complete es 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-// ^ Palidroma
+// ^ Palindroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// * chiedo all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+let palindromeWord = [];
+
+// * creo una funzione per capire se la parola è palindroma (Ritorna VERO o FALSO)
+function isWordPalindrome() {
+	for (let i = 0; i < userWord.length; i++) {
+		palindromeWord.unshift(userWord[i]);
+	}
+	return palindromeWord.join('') === userWord;
+}
+
+console.log(isWordPalindrome());
 
 // ^ Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
